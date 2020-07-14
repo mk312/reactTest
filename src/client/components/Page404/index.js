f0559efda2e757fcdba54a02a729898968740b18
Marina_Kamayeva@tut.by <Marina_Kamayeva@epam.com>
2020-07-14 06:03:21 +0300

    Fix: add lost page

---
+++ src/client/components/Page404/index.js
@@ -0,0 +1,14 @@
+import styles from './styles.scss';
+import React from 'react';
+import { Link } from "react-router-dom";
+
+const Page404 = (props) => (
+    <div className={styles.wrapper}>
+        <p className={styles.text}>Looks like this page doesn't exist. Wrong link or got deleted...</p>
+        <Link  to={'/'} className={styles.link}>
+            GO TO THE MAIN PAGE
+        </Link>
+    </div>
+)
+
+export default Page404;
